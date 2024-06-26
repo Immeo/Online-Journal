6e1007d674dd6a34cf587cf7ef678ab03c51457c
immeo <wayfarer245@gmail.com>
2024-06-26 20:55:08 +0300

    10-change useState to useReducer

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,86 +1,47 @@
 import cn from 'classnames';
-import { useEffect, useState } from 'react';
-import { Button } from '../Buttom/Button';
+import { useEffect, useReducer } from 'react';
+import { Button } from '../Button/Button';
 import styles from './JournalForm.module.css';
-
-const INITIAL_STATE = {
-	title: true,
-	text: true,
-	date: true,
-};
+import { INITIAL_STATE, formReducer } from './JournalForm.state';
 
 export function JournalForm({ addDataItem }) {
-	const [formValidatorState, setFormValidatorState] = useState(INITIAL_STATE);
+	const [formState, disatchForm] = useReducer(formReducer, INITIAL_STATE);
+	const { isValid, isFormReadyToSubmit, values } = formState;
 
 	useEffect(() => {
-		if (
-			!formValidatorState.title ||
-			!formValidatorState.text ||
-			!formValidatorState.date
-		) {
+		let timerIs;
+		if (!isValid.title || !isValid.text || !isValid.date) {
 			setTimeout(() => {
-				setFormValidatorState(INITIAL_STATE);
+				disatchForm({ type: 'RESET_VALIDITY' });
 			}, 3000);
 		}
 		return () => {
-			clearTimeout();
+			clearTimeout(timerIs);
 		};
-	}, [formValidatorState]);
+	}, [isValid]);
 
-	const [inputData, setInputData] = useState('');
-	const handleChange = e => {
-		setInputData(e.target.value);
-	};
+	useEffect(() => {
+		if (isFormReadyToSubmit) {
+			addDataItem(values);
+		}
+	}, [isFormReadyToSubmit]);
 
 	const addNotes = e => {
 		e.preventDefault();
 		const data = new FormData(e.target);
 		const value = Object.fromEntries(data);
-		let isValidForm = true;
-		if (!value.title?.trim().length) {
-			setFormValidatorState(prev => ({
-				...prev,
-				title: false,
-			}));
-			isValidForm = false;
-		} else {
-			setFormValidatorState(prev => ({ ...prev, title: true }));
-		}
-		if (!value.text?.trim().length) {
-			setFormValidatorState(prev => ({
-				...prev,
-				text: false,
-			}));
-			isValidForm = false;
-		} else {
-			setFormValidatorState(prev => ({ ...prev, text: true }));
-		}
-		if (!value.date) {
-			setFormValidatorState(prev => ({
-				...prev,
-				date: false,
-			}));
-			isValidForm = false;
-		} else {
-			setFormValidatorState(prev => ({ ...prev, date: true }));
-		}
-		if (!isValidForm) {
-			return;
-		}
-		addDataItem(value);
+		disatchForm({ type: 'SUBMIT', payload: value });
 	};
 
 	return (
-		<form action='/' className={styles['journal-form']} onSubmit={addNotes}>
+		<form className={styles['journal-form']} onSubmit={addNotes}>
 			<div>
 				<input
 					type='text'
 					name='title'
-					value={inputData}
-					onChange={handleChange}
-					className={cn(styles['form__title__input'], [
-						formValidatorState.title ? '' : styles['journal__form-error'],
-					])}
+					className={cn(styles['form__title__input'], {
+						[styles['journal__form-error']]: !isValid.title,
+					})}
 					placeholder='Title'
 				/>
 			</div>
@@ -93,9 +54,9 @@ export function JournalForm({ addDataItem }) {
 					type='date'
 					name='date'
 					id='form__date'
-					className={cn(styles['form__input'], [
-						formValidatorState.date ? '' : styles['journal__form-error'],
-					])}
+					className={cn(styles['form__input'], {
+						[styles['journal__form-error']]: !isValid.date,
+					})}
 					placeholder='Enter date'
 				/>
 			</div>
@@ -105,9 +66,9 @@ export function JournalForm({ addDataItem }) {
 					<span>Tag</span>
 				</label>
 				<input
-					className={cn(styles['form__input'], [
-						formValidatorState.title ? '' : styles['journal__form-error'],
-					])}
+					className={cn(styles['form__input'], {
+						[styles['journal__form-error']]: !isValid.title,
+					})}
 					name='tag'
 					id='form__tag'
 					placeholder='Enter tag'
@@ -117,9 +78,9 @@ export function JournalForm({ addDataItem }) {
 				name='text'
 				cols='30'
 				rows='10'
-				className={cn(styles['form__input'], [
-					formValidatorState.text ? '' : styles['journal__form-error'],
-				])}
+				className={cn(styles['form__input'], {
+					[styles['journal__form-error']]: !isValid.text,
+				})}
 			></textarea>
 			<Button text='Save' />
 		</form>
